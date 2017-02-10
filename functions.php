82a5191552838375aaf097548835e30f46c838e6
BoomVisibility <web@boomvisibility.com>
2017-02-10 16:29:36 -0500

    Condense text and remove easing

--- functions.php
+++ functions.php
@@ -155,65 +155,33 @@ function twentytwelve_scripts_styles() {
 	   by Open Sans, translate this to 'off'. Do not translate into your own language. */
 
 	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'twentytwelve' ) ) {
-
 		$subsets = 'latin,latin-ext';
-
-
-
 		/* translators: To add an additional Open Sans character subset specific to your language, translate
-
 		   this to 'greek', 'cyrillic' or 'vietnamese'. Do not translate into your own language. */
-
 		$subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'twentytwelve' );
-
-
-
 		if ( 'cyrillic' == $subset )
-
 			$subsets .= ',cyrillic,cyrillic-ext';
-
 		elseif ( 'greek' == $subset )
-
 			$subsets .= ',greek,greek-ext';
-
 		elseif ( 'vietnamese' == $subset )
-
 			$subsets .= ',vietnamese';
-
-
-
 		$protocol = is_ssl() ? 'https' : 'http';
-
 		$query_args = array(
-
 			'family' => 'Open+Sans:400italic,700italic,400,700',
-
 			'subset' => $subsets,
-
 		);
 
 		wp_enqueue_style( 'twentytwelve-fonts', add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" ), array(), null );
-
 	}
 
-
-
 	/*
-
 	 * Loads our main stylesheet.
-
 	 */
 
 	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
-
-
-
 	/*
-
 	 * Loads the Internet Explorer specific stylesheet.
-
 	 */
-
 	wp_enqueue_style( 'twentytwelve-ie', get_template_directory_uri() . '/css/ie.css', array( 'twentytwelve-style' ), '20121010' );
 
 	$wp_styles->add_data( 'twentytwelve-ie', 'conditional', 'lt IE 9' );
@@ -221,8 +189,6 @@ function twentytwelve_scripts_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles' );
-
-
 
 /**
  * Creates a nicely formatted and more specific title element text
@@ -236,48 +202,26 @@ add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles' );
  */
 
 function twentytwelve_wp_title( $title, $sep ) {
-
 	global $paged, $page;
-
-
-
 	if ( is_feed() )
-
 		return $title;
-
-
-
 	// Add the site name.
 
 	$title .= get_bloginfo( 'name' );
-
-
 
 	// Add the site description for the home/front page.
 
 	$site_description = get_bloginfo( 'description', 'display' );
 
 	if ( $site_description && ( is_home() || is_front_page() ) )
-
 		$title = "$title $sep $site_description";
-
-
-
 	// Add a page number if necessary.
-
 	if ( $paged >= 2 || $page >= 2 )
-
 		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentytwelve' ), max( $paged, $page ) );
-
-
-
 	return $title;
-
 }
 
 add_filter( 'wp_title', 'twentytwelve_wp_title', 10, 2 );
-
-
 
 /**
  * Makes our wp_nav_menu() fallback -- wp_page_menu() -- show a home link.
@@ -286,18 +230,12 @@ add_filter( 'wp_title', 'twentytwelve_wp_title', 10, 2 );
  */
 
 function twentytwelve_page_menu_args( $args ) {
-
 	if ( ! isset( $args['show_home'] ) )
-
 		$args['show_home'] = true;
-
 	return $args;
-
 }
 
 add_filter( 'wp_page_menu_args', 'twentytwelve_page_menu_args' );
-
-
 
 /**
  * Registers our main widget area and the front page widget areas.
@@ -421,556 +359,273 @@ register_sidebar( array(
 
 add_action( 'widgets_init', 'twentytwelve_widgets_init' );
 
-
-
 if ( ! function_exists( 'twentytwelve_content_nav' ) ) :
 
 /**
-
  * Displays navigation to next/previous pages when applicable.
-
  *
-
  * @since Twenty Twelve 1.0
-
  */
 
 function twentytwelve_content_nav( $html_id ) {
-
 	global $wp_query;
-
-
-
 	$html_id = esc_attr( $html_id );
-
-
-
 	if ( $wp_query->max_num_pages > 1 ) : ?>
-
 		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
-
 			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-
 			<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
-
 			<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
-
 		</nav><!-- #<?php echo $html_id; ?> .navigation -->
-
 	<?php endif;
-
 }
-
 endif;
-
-
 
 if ( ! function_exists( 'twentytwelve_comment' ) ) :
 
 /**
-
  * Template for comments and pingbacks.
-
  *
-
  * To override this walker in a child theme without modifying the comments template
-
  * simply create your own twentytwelve_comment(), and that function will be used instead.
-
  *
-
  * Used as a callback by wp_list_comments() for displaying the comments.
-
  *
-
  * @since Twenty Twelve 1.0
-
  */
 
 function twentytwelve_comment( $comment, $args, $depth ) {
-
 	$GLOBALS['comment'] = $comment;
-
 	switch ( $comment->comment_type ) :
-
 		case 'pingback' :
-
 		case 'trackback' :
-
 		// Display trackbacks differently than normal comments.
-
 	?>
-
 	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
-
 		<p><?php _e( 'Pingback:', 'twentytwelve' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?></p>
-
 	<?php
-
 			break;
-
 		default :
-
 		// Proceed with normal comments.
-
 		global $post;
-
 	?>
-
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
-
 			<header class="comment-meta comment-author vcard">
-
 				<?php
-
 					echo get_avatar( $comment, 44 );
-
 					printf( '<cite class="fn">%1$s %2$s</cite>',
-
 						get_comment_author_link(),
-
 						// If current post author is also comment author, make it known visually.
-
 						( $comment->user_id === $post->post_author ) ? '<span> ' . __( 'Post author', 'twentytwelve' ) . '</span>' : ''
-
 					);
-
 					printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
-
 						esc_url( get_comment_link( $comment->comment_ID ) ),
-
 						get_comment_time( 'c' ),
-
 						/* translators: 1: date, 2: time */
-
 						sprintf( __( '%1$s at %2$s', 'twentytwelve' ), get_comment_date(), get_comment_time() )
-
 					);
-
 				?>
-
 			</header><!-- .comment-meta -->
-
-
-
 			<?php if ( '0' == $comment->comment_approved ) : ?>
-
 				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentytwelve' ); ?></p>
-
 			<?php endif; ?>
-
-
-
 			<section class="comment-content comment">
-
 				<?php comment_text(); ?>
-
 				<?php edit_comment_link( __( 'Edit', 'twentytwelve' ), '<p class="edit-link">', '</p>' ); ?>
-
 			</section><!-- .comment-content -->
-
-
-
 			<div class="reply">
-
 				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'twentytwelve' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-
 			</div><!-- .reply -->
-
 		</article><!-- #comment-## -->
-
 	<?php
-
 		break;
-
 	endswitch; // end comment_type check
-
 }
-
 endif;
 
-
-
 if ( ! function_exists( 'twentytwelve_entry_meta' ) ) :
-
 /**
-
  * Prints HTML with meta information for current post: categories, tags, permalink, author, and date.
-
  *
-
  * Create your own twentytwelve_entry_meta() to override in a child theme.
-
  *
-
  * @since Twenty Twelve 1.0
-
  */
 
 function twentytwelve_entry_meta() {
-
 	// Translators: used between list items, there is a space after the comma.
-
 	$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
-
-
-
 	// Translators: used between list items, there is a space after the comma.
-
 	$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
-
-
-
 	$date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>',
-
 		esc_url( get_permalink() ),
-
 		esc_attr( get_the_time() ),
-
 		esc_attr( get_the_date( 'c' ) ),
-
 		esc_html( get_the_date() )
-
 	);
-
-
-
 	$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-
 		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-
 		esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
-
 		get_the_author()
-
 	);
-
-
 
 	// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
-
 	if ( $tag_list ) {
-
 		$utility_text = __( 'This entry was posted in %1$s and tagged %2$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-
 	} elseif ( $categories_list ) {
-
 		$utility_text = __( 'This entry was posted in %1$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-
 	} else {
-
 		$utility_text = __( 'This entry was posted on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-
 	}
 
-
-
 	printf(
-
 		$utility_text,
-
-		$categories_list,
-
+		$categories_list
 		$tag_list,
-
 		$date,
-
 		$author
-
 	);
-
 }
-
 endif;
 
-
-
 /**
-
  * Extends the default WordPress body class to denote:
-
  * 1. Using a full-width layout, when no active widgets in the sidebar
-
  *    or full-width template.
-
  * 2. Front Page template: thumbnail in use and number of sidebars for
-
  *    widget areas.
-
  * 3. White or empty background color to change the layout and spacing.
-
  * 4. Custom fonts enabled.
-
  * 5. Single or multiple authors.
-
  *
-
  * @since Twenty Twelve 1.0
-
  *
-
  * @param array Existing class values.
-
  * @return array Filtered class values.
-
  */
 
 function twentytwelve_body_class( $classes ) {
-
 	$background_color = get_background_color();
 
-
-
 	if ( ! is_active_sidebar( 'sidebar-1' ) || is_page_template( 'page-templates/full-width.php' ) )
-
 		$classes[] = 'full-width';
 
-
-
 	if ( is_page_template( 'page-templates/front-page.php' ) ) {
-
 		$classes[] = 'template-front-page';
-
 		if ( has_post_thumbnail() )
-
 			$classes[] = 'has-post-thumbnail';
-
 		if ( is_active_sidebar( 'sidebar-2' ) && is_active_sidebar( 'sidebar-3' ) )
-
 			$classes[] = 'two-sidebars';
-
 	}
 
-
-
 	if ( empty( $background_color ) )
-
 		$classes[] = 'custom-background-empty';
-
 	elseif ( in_array( $background_color, array( 'fff', 'ffffff' ) ) )
-
 		$classes[] = 'custom-background-white';
 
-
-
 	// Enable custom font class only if the font CSS is queued to load.
-
 	if ( wp_style_is( 'twentytwelve-fonts', 'queue' ) )
-
 		$classes[] = 'custom-font-enabled';
-
-
-
 	if ( ! is_multi_author() )
-
 		$classes[] = 'single-author';
-
-
-
 	return $classes;
-
 }
 
 add_filter( 'body_class', 'twentytwelve_body_class' );
 
-
-
 /**
-
  * Adjusts content_width value for full-width and single image attachment
-
  * templates, and when there are no active widgets in the sidebar.
-
  *
-
  * @since Twenty Twelve 1.0
-
  */
 
 function twentytwelve_content_width() {
-
 	if ( is_page_template( 'page-templates/full-width.php' ) || is_attachment() || ! is_active_sidebar( 'sidebar-1' ) ) {
-
 		global $content_width;
-
 		$content_width = 960;
-
 	}
-
 }
 
 add_action( 'template_redirect', 'twentytwelve_content_width' );
 
-
-
 /**
-
  * Add postMessage support for site title and description for the Theme Customizer.
-
  *
-
  * @since Twenty Twelve 1.0
-
  *
-
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
-
  * @return void
-
  */
 
 function twentytwelve_customize_register( $wp_customize ) {
-
 	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
-
 	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
-
 }
 
 add_action( 'customize_register', 'twentytwelve_customize_register' );
 
-
-
 /**
-
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
-
  *
-
  * @since Twenty Twelve 1.0
-
  */
 
 function twentytwelve_customize_preview_js() {
-
 	wp_enqueue_script( 'twentytwelve-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20120827', true );
-
 }
 
 add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
 
-
-
-
-
-
-
 /**add scripts to header*/
 
-
-
 function my_scripts_method() {
-
 	wp_enqueue_script(
-
 		'custom-slider',
-
 		get_stylesheet_directory_uri() . '/js/jquery.bxslider.min.js',
-
 		array( 'jquery' ),
 		1.1,
 		true
-
 	);
 
-	
-
 	wp_enqueue_script(
-
 		'image-lightbox',
-
 		get_stylesheet_directory_uri() . '/js/imagelightbox.js',
-
 		array( 'jquery' ),
-
 		1.1,
-
 		true
-
 	);
 	
 	wp_enqueue_script(
-
 		'nav-menu',
-
 		get_stylesheet_directory_uri() . '/js/jquery.slicknav.js',
-
-		array( 'jquery' ),
-		
+		array( 'jquery' ),		
 		1.1,
-
 		true
-
 	);
 	
 	wp_enqueue_script(
-
 		'modernizer',
-
 		get_stylesheet_directory_uri() . '/js/modernizr.min.js',
-
-		array( 'jquery' ),
-		
+		array( 'jquery' ),		
 		1.1,
-
 		true
-
 	);
 
-	
-
 	wp_enqueue_script(
-
 		'custom',
-
 		get_stylesheet_directory_uri() . '/js/custom.js',
-
-		array( 'jquery' ),
-		
+		array( 'jquery' ),		
 		1.1,
-
 		true
-
 	);
-
-	
-
-	wp_enqueue_script(
-
-		'easing',
-
-		'http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js',
-
-		array( 'jquery' ),
-		
-		1.1,
-
-		true
-
-	);
-
-	
-
 }
-
-
 
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 
-
-
 wp_enqueue_style( 'bxslider', get_template_directory_uri() . '/css/jquery.bxslider.css' ); //our stylesheet
 
-
-
-
-
 /**custom post type for slider*/
-
-
-
 register_post_type('slides', array(
 'label' => __('Slides'),
 'singular_label' => __('Slides'),
@@ -985,7 +640,6 @@ register_post_type('slides', array(
 ));
 
 /**custom post type for testimonials*/
-
 register_post_type('testimonials', array(
 'label' => __('Testimonials'),
 'singular_label' => __('Testimonial'),
@@ -998,7 +652,7 @@ register_post_type('testimonials', array(
 'taxonomies' => array( 'post_tag')
 ));
 
-register_post_type('faq', array(
+/**custom post type for FAQs */
 'label' => __('FAQs'),
 'singular_label' => __('FAQ'),
 'public' => true,
@@ -1012,70 +666,40 @@ register_post_type('faq', array(
 'taxonomies' => array( 'post_tag')
 ));
 
-
 /**add slider thumbnail size*/
-
-
-
 if ( function_exists( 'add_image_size' ) ) { 
 	//add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
 	add_image_size( 'homepage-slider', 1600, 915, true ); //(cropped)
 	add_image_size( 'blog-thumb', 360, 360, true ); //(cropped)
 }
 
-
 function filter_ptags_on_images($content){
-
    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-
 }
-
-
 
 add_filter('the_content', 'filter_ptags_on_images');
-
 function iljs_mod_tags ($content) {
-
    global $post;
-
    $type="f"; // the type of imagelightbox, f: combined
-
-
-
    $pattern = "/<a(.*?)href=('|\")(.*?).(bmp|gif|jpeg|jpg|png)('|\")(.*?)>/i";
-
    $replacement = '<a$1href=$2$3.$4$5$6 data-imagelightbox="'.$type.'">';
-
    $content = preg_replace($pattern, $replacement, $content);
 
-
-
    return $content;
-
 }
 
-
-
-      // adds the filter for image galleries
-
+// adds the filter for image galleries
 add_filter('wp_get_attachment_link','iljs_mod_tags');
 
 // adds the filter for single content images
 add_filter('the_content', 'iljs_mod_tags');
 
-
-
 function custom_excerpt_length( $length ) {
-
 	return 30;
-
 }
-
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
-
 //add columns shortcodes
-
 function webtreats_one_third( $atts, $content = null ) {
    return '<div class="one_third">' . do_shortcode($content) . '</div>';
 }
