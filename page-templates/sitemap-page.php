cd4fe9a56222e1ae7a8e4ce72bf13e90bfcb4052
BoomVisibility <web@boomvisibility.com>
2018-05-15 16:04:45 -0400

    Add WP_Query to posts loop

--- page-templates/sitemap-page.php
+++ page-templates/sitemap-page.php
@@ -34,27 +34,29 @@ wp_list_pages(
 </ul>
 
 <h2 id="posts">Posts</h2>
-<ul>
-<?php
-// Add categories you'd like to exclude in the exclude here
-$cats = get_categories('exclude=');
-foreach ($cats as $cat) {
-  echo "<h3>".$cat->cat_name."</h3>";
-  echo "<ul>";
-  query_posts('posts_per_page=-1&cat='.$cat->cat_ID);
-  while(have_posts()) {
-    the_post();
-    $category = get_the_category();
-    // Only display a post link once, even if it's in multiple categories
-    if ($category[0]->cat_ID == $cat->cat_ID) {
-      echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-    }
-  }
-  echo "</ul>";
-  echo "</li>";
-}
-?>
-</ul>
+				<ul>
+				<?php
+				// Add categories you'd like to exclude in the exclude here
+				$cats = get_categories('exclude=');
+				foreach ($cats as $cat) {
+						echo "<h3>".$cat->cat_name."</h3>";
+						echo "<ul>";
+						$the_query = new WP_Query('posts_per_page=-1&cat='.$cat->cat_ID);
+						if ( $the_query->have_posts() ) {
+								while ( $the_query->have_posts() ) {
+									$the_query->the_post();
+								$category = get_the_category();
+								// Only display a post link once, even if it's in multiple categories
+								if ($category[0]->cat_ID == $cat->cat_ID) {
+										echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+									}
+								}
+							echo "</ul>";
+						}
+						wp_reset_postdata();
+					}
+				?>
+				</ul>
 
 				
 				<h2><a href="/sitemap_index.xml">XML Sitemap</a></h2>
